8e4b4c6044d9e7637892f4192f27342b494020b2
Kaue Miyauti Machado <kaumac@gmail.com>
2023-04-18 04:09:14 -0300

    fix lint

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,31 +1,42 @@
-import { IRoute } from 'types/navigation';
-import Router from 'next/router';
+// import Router from 'next/router'
+// import { IRoute } from 'types/navigation'
 
-// NextJS Requirement
-export const isWindowAvailable = () => typeof window !== 'undefined';
+// // NextJS Requirement
+// export const isWindowAvailable = () => typeof window !== 'undefined'
 
-export const findCurrentRoute = (routes: IRoute[], pathname: string): IRoute => {
-  if (!isWindowAvailable()) return null;
+// export const findCurrentRoute = (
+//   routes: IRoute[],
+//   pathname: string
+// ): IRoute => {
+//   if (!isWindowAvailable()) return null
 
-  for (let route of routes) {
-    if (!!route.items) {
-      const found = findCurrentRoute(route.items, pathname);
-      if (!!found) return found;
-    }
-    if (pathname?.match(route.path) && route) return route;
-  }
-};
+//   for (let route of routes) {
+//     if (!!route.items) {
+//       const found = findCurrentRoute(route.items, pathname)
+//       if (!!found) return found
+//     }
+//     if (pathname?.match(route.path) && route) return route
+//   }
+// }
 
-export const getActiveRoute = (routes: IRoute[], pathname: string): string => {
-  const route = findCurrentRoute(routes, pathname);
-  return route?.name || 'Default Brand Text';
-};
+// export const getActiveRoute = (routes: IRoute[], pathname: string): string => {
+//   const route = findCurrentRoute(routes, pathname)
+//   return route?.name || 'Default Brand Text'
+// }
 
-export const getActiveNavbar = (routes: IRoute[], pathname: string): boolean => {
-  const route = findCurrentRoute(routes, pathname);
-  return route?.secondary;
-};
+// export const getActiveNavbar = (
+//   routes: IRoute[],
+//   pathname: string
+// ): boolean => {
+//   const route = findCurrentRoute(routes, pathname)
+//   return route?.secondary
+// }
 
-export const getActiveNavbarText = (routes: IRoute[], pathname: string): string | boolean => {
-  return getActiveRoute(routes, pathname) || false;
-};
+// export const getActiveNavbarText = (
+//   routes: IRoute[],
+//   pathname: string
+// ): string | boolean => {
+//   return getActiveRoute(routes, pathname) || false
+// }
+
+export {}
