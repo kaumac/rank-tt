47ae2dee0a78025652ebb12b66ff44ac6ffcb362
Kaue Miyauti Machado <kaumac@gmail.com>
2023-05-08 01:55:55 -0300

    wip group functionality

--- src/app/painel/torneios/[tournamentId]/components/PlayersTab/components/CreateGroupWrapper.js
+++ src/app/painel/torneios/[tournamentId]/components/PlayersTab/components/CreateGroupWrapper.js
@@ -18,10 +18,17 @@ import {
   Th,
   Tbody,
   Td,
-  Box
+  Box,
+  Flex,
+  Center,
+  Card,
+  Heading,
+  Text,
+  Divider
 } from '@chakra-ui/react'
 import { arrayUnion, collection, query, where } from 'firebase/firestore'
 import React, { useState } from 'react'
+import { BiTrash, BiPlus } from 'react-icons/bi'
 
 import firebaseUpdateDoc from '@/firebase/updateDoc'
 import { groupCollectionDocsByField } from '@/firebase/utils'
@@ -46,6 +53,12 @@ export const CreateGroupWrapper = ({
   )
 
   const categoryGroups = groupsGroupedByCategory[categoryId]
+
+  const unassignedPlayers = players.filter(
+    (player) =>
+      !player.group &&
+      !newGroup.players.find((newGroupPlayer) => newGroupPlayer === player.id)
+  )
 
   console.log({ categoryGroups })
 
@@ -105,80 +118,163 @@ export const CreateGroupWrapper = ({
   return (
     <>
       {renderChildren()}
-      <Modal isOpen={isOpen} onClose={onClose} isCentered size="lg">
+      <Modal isOpen={isOpen} onClose={onClose} isCentered size="xl">
         <ModalOverlay />
-        <ModalContent p={4}>
+        <ModalContent p={4} maxWidth="80vw">
           <ModalHeader>
             Criar grupo{' '}
             {newGroup.length > 0 && `(${newGroup.length} selecionados)`}
           </ModalHeader>
           <ModalCloseButton />
           <ModalBody px={0}>
-            <Box overflowY="auto" maxHeight="calc(100vh - 280px)">
-              <TableContainer width="100%">
-                <Table variant="simple">
-                  {/* <TableCaption>Imperial to metric conversion factors</TableCaption> */}
-                  <Thead>
-                    <Tr>
-                      <Th>Nome</Th>
-                      <Th>Adicionar</Th>
-                    </Tr>
-                  </Thead>
-                  <Tbody>
-                    {newGroup.players
-                      .filter((newGroupPlayer) => newGroupPlayer === 'bye')
-                      .map((bye, byeIndex) => (
-                        <Tr key={`group-bye-list-${byeIndex}`}>
-                          <Td>Bye</Td>
-                          <Td>
-                            <Button
-                              variant="ghost"
-                              colorScheme="red"
-                              onClick={() => {
-                                handleRemovePlayer('bye')
-                              }}
-                            >
-                              remover
-                            </Button>
-                          </Td>
-                        </Tr>
-                      ))}
-                    {(players || []).map((player) => {
-                      const playerData = player.data()
+            <Flex>
+              <Box pl={6} pr={8} minWidth="50%">
+                <Text color="gray.500" mb={2}>
+                  Selecione os jogadores na lista para criar um novo grupo
+                </Text>
+                <Card p={4}>
+                  <Heading color="gray.700" size="md" mb={8}>
+                    Grupo 01
+                  </Heading>
+                  <TableContainer width="100%">
+                    <Table variant="simple">
+                      <Tbody>
+                        {newGroup.players.map(
+                          (newGroupPlayer, newGroupPlayerIndex) => {
+                            playerData = players
+                              .find((player) => player.id === newGroupPlayer)
+                              .data()
 
-                      return (
-                        <Tr key={player.id}>
-                          <Td>{playerData.name}</Td>
-                          <Td>
-                            {newGroup.players.includes(player.id) ? (
+                            console.log(playerData)
+
+                            return (
+                              <Tr
+                                key={`group-player-list-${newGroupPlayerIndex}`}
+                              >
+                                <Td
+                                  position="relative"
+                                  data-group
+                                  borderBottom={
+                                    newGroupPlayerIndex + 1 ===
+                                    newGroup.players.length
+                                      ? 'none'
+                                      : null
+                                  }
+                                >
+                                  {playerData?.name}
+                                  <Center
+                                    width="100%"
+                                    height="100%"
+                                    position="absolute"
+                                    top="0"
+                                    left="0"
+                                    cursor="pointer"
+                                    opacity="0"
+                                    transition="opacity 0.2s ease-in-out"
+                                    _groupHover={{
+                                      bg: 'rgba(201, 41, 31, 0.04)',
+                                      opacity: 1
+                                    }}
+                                  >
+                                    <Button
+                                      colorScheme="red"
+                                      leftIcon={<BiTrash />}
+                                    >
+                                      Remover do grupo
+                                    </Button>
+                                  </Center>
+                                </Td>
+                              </Tr>
+                            )
+                          }
+                        )}
+                      </Tbody>
+                    </Table>
+                  </TableContainer>
+                </Card>
+              </Box>
+              <Box overflowY="auto" maxHeight="calc(100vh - 280px)" flex="1">
+                <TableContainer width="100%">
+                  <Table variant="simple">
+                    <Tbody>
+                      {newGroup.players
+                        .filter((newGroupPlayer) => newGroupPlayer === 'bye')
+                        .map((bye, byeIndex) => (
+                          <Tr key={`group-bye-list-${byeIndex}`}>
+                            <Td>Bye</Td>
+                            <Td>
                               <Button
                                 variant="ghost"
                                 colorScheme="red"
                                 onClick={() => {
-                                  handleRemovePlayer(player.id)
+                                  handleRemovePlayer('bye')
                                 }}
                               >
                                 remover
                               </Button>
-                            ) : (
-                              <Button
-                                variant="ghost"
-                                colorScheme="green"
-                                onClick={() => {
-                                  handleAddPlayer(player.id)
+                            </Td>
+                          </Tr>
+                        ))}
+                      {(unassignedPlayers || []).map((player) => {
+                        const playerData = player.data()
+
+                        return (
+                          <Tr key={player.id}>
+                            <Td position="relative" data-group>
+                              {playerData.name}
+                              <Center
+                                width="100%"
+                                height="100%"
+                                position="absolute"
+                                top="0"
+                                left="0"
+                                cursor="pointer"
+                                opacity="0"
+                                transition="opacity 0.2s ease-in-out"
+                                _groupHover={{
+                                  bg: 'rgba(85, 31, 201, 0.04)',
+                                  opacity: 1
                                 }}
                               >
-                                adicionar
-                              </Button>
-                            )}
-                          </Td>
-                        </Tr>
-                      )
-                    })}
-                  </Tbody>
-                </Table>
-              </TableContainer>
-            </Box>
+                                <Button
+                                  leftIcon={<BiPlus />}
+                                  onClick={() => {
+                                    handleAddPlayer(player.id)
+                                  }}
+                                >
+                                  Adicionar ao grupo
+                                </Button>
+                              </Center>
+                            </Td>
+                            {/* <Td>
+                              {newGroup.players.includes(player.id) ? (
+                                <Button
+                                  variant="ghost"
+                                  colorScheme="red"
+                                  onClick={() => {
+                                    handleRemovePlayer(player.id)
+                                  }}
+                                >
+                                  remover
+                                </Button>
+                              ) : (
+                                <Button
+                                  variant="ghost"
+                                  colorScheme="green"
+                                  
+                                >
+                                  adicionar
+                                </Button>
+                              )}
+                            </Td> */}
+                          </Tr>
+                        )
+                      })}
+                    </Tbody>
+                  </Table>
+                </TableContainer>
+              </Box>
+            </Flex>
           </ModalBody>
 
           <ModalFooter>
