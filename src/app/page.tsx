2564e853c74f0e01d8e04623ca41dd012dec6d00
Kaue Miyauti Machado <kaumac@gmail.com>
2023-06-19 02:29:14 -0300

    wip init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+
+import AuthForm from "@/components/AuthForm";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Get started by editing&nbsp; sdkfdsfodsfkodsfkodsksdfo
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
@@ -15,7 +17,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -38,6 +40,8 @@ export default function Home() {
           priority
         />
       </div>
+
+      <AuthForm />
 
       <div className={styles.grid}>
         <a
@@ -91,5 +95,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
