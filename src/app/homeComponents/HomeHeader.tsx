bbf82418b762ad28f6a7ccb55accb71880fb5362
Kaue Miyauti Machado <kaumac@gmail.com>
2023-06-19 04:11:26 -0300

    wip initial setup

---
+++ src/app/homeComponents/HomeHeader.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { Box, Button } from '@chakra-ui/react'
+
+export const HomeHeader = () => {
+  return (
+    <main>
+      sdfkodskodskodsfkods
+      <Box>
+        <Button colorScheme="red">dfdsfdss</Button>
+        <Button colorScheme="green">dfdsfdss</Button>
+        <Button colorScheme="orange">dfdsfdss</Button>
+        <Button colorScheme="yellow">dfdsfdss</Button>
+        <Button colorScheme="blue">dfdsfdss</Button>
+        <Button colorScheme="">dfdsfdss</Button>
+        <Button colorScheme="">dfdsfdss</Button>
+        <Button colorScheme="">dfdsfdss</Button>
+        <Button colorScheme="">dfdsfdss</Button>
+        <Button colorScheme="">dfdsfdss</Button>
+        <Button colorScheme="">dfdsfdss</Button>
+      </Box>
+    </main>
+  )
+}
+
+export default HomeHeader
---
+++ src/theme/index.ts
@@ -0,0 +1,33 @@
+import { extendTheme, withDefaultColorScheme } from '@chakra-ui/react'
+
+import colors from './colors'
+
+// import { ButtonComponent } from './additions/button/button'
+// import { NewCard } from './additions/card/card'
+// import { badgeStyles } from './components/badge'
+// import { buttonStyles } from './components/button'
+// import { inputStyles } from './components/input'
+// import { linkStyles } from './components/link'
+// import { progressStyles } from './components/progress'
+// import { sliderStyles } from './components/slider'
+// import { switchStyles } from './components/switch'
+// import { textareaStyles } from './components/textarea'
+// import { breakpoints } from './foundations/breakpoints'
+// import { globalStyles } from './styles'
+
+export default extendTheme(
+  {
+    colors
+  }, // Breakpoints
+  // badgeStyles, // badge styles
+  // buttonStyles, // button styles
+  // linkStyles, // link styles
+  // progressStyles, // progress styles
+  // sliderStyles, // slider styles
+  // inputStyles, // input styles
+  // textareaStyles, // textarea styles
+  // switchStyles, // switch styles
+  // NewCard, // card component
+  // ButtonComponent, // card component
+  withDefaultColorScheme({ colorScheme: 'brand' })
+)
